c64557a789e90ec3223b57f2b6a2f96983e24f2a
Aleksandr Andriienko <oandriie@redhat.com>
2017-09-20 19:22:18 +0300

    Fix up.
    Signed-off-by: Aleksandr Andriienko <oandriie@redhat.com>

--- ide/che-core-ide-api/src/main/java/org/eclipse/che/ide/api/git/GitServiceClientImpl.java
+++ ide/che-core-ide-api/src/main/java/org/eclipse/che/ide/api/git/GitServiceClientImpl.java
@@ -440,14 +440,14 @@ public class GitServiceClientImpl implements GitServiceClient {
   }
 
   @Override
-  public Promise<List<EditedRegion>> getEditedRegions(Path project, String filePath) {
+  public Promise<List<EditedRegion>> getEditedRegions(Path project, Path filePath) {
     String url =
         getWsAgentBaseUrl()
             + EDITS
             + "?projectPath="
             + encodePath(project)
             + "&filePath="
-            + encodePath(valueOf(filePath));
+            + encodePath(filePath);
     return asyncRequestFactory
         .createGetRequest(url)
         .send(dtoUnmarshallerFactory.newListUnmarshaller(EditedRegion.class));
